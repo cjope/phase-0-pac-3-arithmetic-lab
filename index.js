ae2dbfce9ee81407f8868214b8b2438a9ddd4b6c
cjope <craigjope@gmail.com>
2021-05-21 17:03:38 -0600

    Completed assignment

---
+++ index.js
@@ -0,0 +1,37 @@
+var n = 100;
+var a = 50
+var b = 25
+
+function add() {
+}
+function subtract() {
+}
+function multiply() {
+}
+function divide() {
+}
+function add(a,b) {
+    return a+b;
+}
+function subtract(a,b) {
+    return a-b ;
+}
+function multiply(a,b) {
+    return a*b;
+}
+function divide(a,b) {
+    return a/b ;
+}
+function increment(n) {
+    return ++n;
+}
+function decrement(n) {
+    return --n;
+}
+function makeInt(n) {
+    return parseInt(n,10);
+}
+function preserveDecimal(n) {
+    return parseFloat(n, 10.0)
+    return parseInt(n, 10.0);
+}
